7478f60aeec9817339b97a78577a2b060c971071
Alan victor <alanvictordossantos337@gmail.com>
2024-04-05 21:38:48 -0300

    programas

--- routers/cadastrar.js
+++ routers/cadastrar.js
@@ -6,6 +6,13 @@ router.get('/', (req, res) => {
     res.render('cadastro');
 });
 
+function validarDataNascimento(data){
+    const dataNascimento = new Date(data);
+    const dataAtual = new Date();
+    return dataNascimento < dataAtual;
+}
+
+
 
 
 module.exports = router;
